520a5acefb8630ce736dea7b9566e341ddb333a2
Stnqls2 <limser05@gmail.com>
2025-02-24 22:11:07 +0900

    20250220
    별 애니메이션 구현

--- ClearScreen.js
+++ ClearScreen.js
@@ -8,16 +8,3 @@ function ScreenSize() {
     const container = document.querySelector(".game-clear-container");
     container.style.height = window.innerHeight + "px"; 
 }
-
-
-
-/* 별 애니메이션 */
-const stars = document.querySelectorAll('.star1, .star2, .star3, .star4, .star5'); 
-
-stars.forEach((star, index) => {
-    setTimeout(() => {
-        star.style.opacity = "1";  
-        star.style.transform = "rotate(360deg) scale(1.2)"; 
-        star.style.transition = "transform 1s ease-in-out, opacity 0.5s ease-in";  
-    }, index * 200);  
-});
