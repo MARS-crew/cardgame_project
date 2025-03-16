464f0707a36b78e50722e4c9dd0fcf87f07cd52b
JinHo <cse.hodan@gmail.com>
2025-03-16 22:52:18 +0900

    GameMainScreen입니다.

---
+++ MatchTheCard_GameMainScreen/MainScreen.js
@@ -0,0 +1,21 @@
+function startGame() {
+    const nickname = document.getElementById('nickname').value;
+    if (nickname) {
+        window.location.href="../../MatchTheCard_GamePlayScreen/GameScreen.html"
+    } else {
+        alert('닉네임을 입력해 주세요.');
+    }
+}
+function openGameGuide() {
+    document.getElementById('gameGuideModal').style.display = "block";
+}
+function closeGameGuide() {
+    document.getElementById('gameGuideModal').style.display = "none";
+}
+// 모달 닫기
+window.onclick = function(event) {
+    const modal = document.getElementById('gameGuideModal');
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
